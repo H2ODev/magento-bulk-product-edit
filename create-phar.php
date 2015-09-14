38d919bfa42455354edf79fe50783b8a100c28ca
Marvin Oßwald <mo@h2om.de>
2015-09-14 14:15:21 +0200

    fixed typo

--- create-phar.php
+++ create-phar.php
@@ -5,5 +5,5 @@ $buildRoot = getenv('CIRCLE_ARTIFACTS');
 $phar = new Phar($buildRoot . "/BuldProductEdit.phar", 
 	FilesystemIterator::CURRENT_AS_FILEINFO |     	FilesystemIterator::KEY_AS_FILENAME, "BuldProductEdit.phar");
 $phar["index.php"] = file_get_contents($srcRoot . "/index.php");
-$phar["common.php"] = file_get_contents($srcRoot . "/bulkedit.php");
+$phar["bulkedit.php"] = file_get_contents($srcRoot . "/bulkedit.php");
 $phar->setStub($phar->createDefaultStub("index.php"));
